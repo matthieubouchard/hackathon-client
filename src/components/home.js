2878d79a8631d1fd73dc2f0248a0af9d43ddb98b
matthewbouchard <bouchard.matthewj@gmail.com>
2020-02-28 07:09:05 -0700

    hit api

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
+  const hitApi = async () => {
+    const response = await axios("http://localhost:5000/");
+    if(response.data.success) {
+      alert("success");
+    }
+  };
+
   return (
     <div>
       <h1>HOME</h1>
       <Link to="/candidates">Candidates</Link>
+      <br/>
+      <button onClick={hitApi}>test api</button>
     </div>
   );
 }
