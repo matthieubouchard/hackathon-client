28bd08fa5c53b0cc00f29a1b84b9dff5039a9303
matthewbouchard <bouchard.matthewj@gmail.com>
2020-02-28 09:42:13 -0700

    add images

--- src/components/candidates.js
+++ src/components/candidates.js
@@ -1,14 +1,68 @@
 import React from "react";
 
+import { ppl } from "./people";
+
 import { Link } from "react-router-dom";
 
-function Candidates() {
+const imageStyles = {
+  width: "150px",
+  borderRadius: "50%"
+};
+
+const containerStyles = {
+  marginTop: "200px"
+};
+
+const candidateStyles = {
+  display: "inline-flex ",
+  justifyContent: 'space-around',
+  width: '85%',
+}
+
+console.log("peoppe?", ppl);
+function Candidate({ candidate }) {
+  console.log("CANDIDATE", candidate);
+
   return (
-    <div>
-      <h1>Candidates</h1>
-      <Link to="/topics">Topics</Link>
+    <div style={containerStyles}>
+      <img
+        style={imageStyles}
+        className="person"
+        src={candidate.img}
+        alt="candidate"
+      />
+      <label>{candidate.name}</label>
     </div>
   );
+}
+
+class Candidates extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      candidates: []
+    }
+  }
+
+  selectPerson = (e) => {
+    console.log('e', e)
+
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="candidates" style={candidateStyles}>
+          {ppl.length && ppl.map(person => <Candidate onClick={this.selectPerson}  key={person.name} candidate={person} />)}
+        </div>
+
+        <h1>Candidates</h1>
+        <Link to="/topics">Topics</Link>
+      </div>
+    );
+  }
 }
 
 export default Candidates;
---
+++ src/components/people.js
@@ -0,0 +1,23 @@
+export const ppl = [
+  { name: "sanders", img: "../../Sanders.png" },
+  {
+    name: "warren",
+    img: "../../Warren.png"
+  },
+  {
+    name: "bloomberg",
+    img: "../../Bloomberg.png"
+  },
+  {
+    name: "klobuchar",
+    img: "../../Klobuchar.png"
+  },
+  {
+    name: "biden",
+    img: "../../Biden.png"
+  },
+  {
+    name: "buttigeg",
+    img: "../../Buttigieg.png"
+  }
+];
