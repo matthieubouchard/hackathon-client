32c8afcb3ee9194c27e508432c88169969afb6f9
Stephen Hathaway <stphnhthwy@gmail.com>
2020-02-29 13:07:13 -0700

    style updates

--- src/components/home.js
+++ src/components/home.js
@@ -17,7 +17,7 @@ function Home() {
       <Link to="/candidates">Candidates</Link>
       <br/>
       <button onClick={hitApi}>test api</button>
-      <p className="font-test-poster">Poster test</p>
+      <p className="font-test-poster">Poster</p>
       <p className="font-test-extra-bold">Extra Bold test</p>
       <p className="font-test-medium">Medium test</p>
     </div>
